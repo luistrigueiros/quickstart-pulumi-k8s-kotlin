ae8ffad423f78deb84941db8ca55c32e9a7a12a1
Luis Trigueiros <luistrigueiros@gmail.com>
2022-08-11 19:35:08 +0100

    Incremental save

--- src/main/kotlin/myproject/Main.kt
+++ src/main/kotlin/myproject/Main.kt
@@ -5,16 +5,14 @@ import com.pulumi.Pulumi
 import com.pulumi.kubernetes.meta_v1.outputs.ObjectMeta
 import java.util.*
 
-fun run(ctx:Context) {
+fun run(ctx: Context) {
     val deployment = getDeployment(mapOf("app" to "nginx"))
     val name = deployment.metadata()
         .applyValue { m: Optional<ObjectMeta> -> m.orElseThrow().name().orElse("") }
     ctx.export("name", name)
 }
 
-object Main {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        Pulumi.run(::run)
-    }
+fun main(args: Array<String>) {
+    Pulumi.run(::run)
 }
+
