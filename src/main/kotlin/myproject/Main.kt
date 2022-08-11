9d0550674b2f116c50848c25714e5ea8ce4d4a33
Luis Trigueiros <luistrigueiros@gmail.com>
2022-08-11 19:27:05 +0100

    Converted code to java

--- src/main/kotlin/myproject/Main.kt
+++ src/main/kotlin/myproject/Main.kt
@@ -1,41 +1,20 @@
-package myproject;
+package myproject
 
-import static myproject.SupportKt.getTemplate;
+import com.pulumi.Context
+import com.pulumi.Pulumi
+import com.pulumi.kubernetes.meta_v1.outputs.ObjectMeta
+import java.util.*
 
-import com.pulumi.Pulumi;
-import com.pulumi.core.Output;
-import com.pulumi.kubernetes.apps_v1.Deployment;
-import com.pulumi.kubernetes.apps_v1.DeploymentArgs;
-import com.pulumi.kubernetes.apps_v1.inputs.DeploymentSpecArgs;
-import com.pulumi.kubernetes.meta_v1.inputs.LabelSelectorArgs;
-import java.util.Map;
+fun run(ctx:Context) {
+    val deployment = getDeployment(mapOf("app" to "nginx"))
+    val name = deployment.metadata()
+        .applyValue { m: Optional<ObjectMeta> -> m.orElseThrow().name().orElse("") }
+    ctx.export("name", name)
+}
 
-public class Main {
-
-    private static Deployment getDeployment(Map labels) {
-        return new Deployment("nginx", DeploymentArgs.builder()
-            .spec(DeploymentSpecArgs.builder()
-                .selector(LabelSelectorArgs.builder()
-                    .matchLabels(labels)
-                    .build())
-                .replicas(1)
-                .template(getTemplate(labels))
-
-                .build())
-            .build());
-    }
-
-
-    public static void main(String[] args) {
-        Pulumi.run(ctx -> {
-            Map labels = Map.of("app", "nginx");
-
-            Deployment deployment = getDeployment(labels);
-
-            Output<Object> name = deployment.metadata()
-                .applyValue(m -> m.orElseThrow().name().orElse(""));
-
-            ctx.export("name", name);
-        });
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Pulumi.run(::run)
     }
 }
--- src/main/kotlin/myproject/Support.kt
+++ src/main/kotlin/myproject/Support.kt
@@ -1,9 +1,13 @@
 package myproject
 
+import com.pulumi.kubernetes.apps_v1.Deployment
+import com.pulumi.kubernetes.apps_v1.DeploymentArgs
+import com.pulumi.kubernetes.apps_v1.inputs.DeploymentSpecArgs
 import com.pulumi.kubernetes.core_v1.inputs.ContainerArgs
 import com.pulumi.kubernetes.core_v1.inputs.ContainerPortArgs
 import com.pulumi.kubernetes.core_v1.inputs.PodSpecArgs
 import com.pulumi.kubernetes.core_v1.inputs.PodTemplateSpecArgs
+import com.pulumi.kubernetes.meta_v1.inputs.LabelSelectorArgs
 import com.pulumi.kubernetes.meta_v1.inputs.ObjectMetaArgs
 
 fun getContainers(): PodSpecArgs.Builder = PodSpecArgs.builder().containers(
@@ -30,5 +34,22 @@ fun getTemplate(labels: Map<String, String>): PodTemplateSpecArgs =
                 .build()
         ).build()
 
+
+fun getDeployment(labels: Map<String, String>): Deployment =
+    Deployment(
+        "nginx", DeploymentArgs.builder()
+            .spec(
+                DeploymentSpecArgs.builder()
+                    .selector(
+                        LabelSelectorArgs.builder()
+                            .matchLabels(labels)
+                            .build()
+                    )
+                    .replicas(1)
+                    .template(getTemplate(labels))
+                    .build()
+            )
+            .build()
+    )
 
 
