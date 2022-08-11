541290aeaa7f5c41af6865c2dd305503afaf0c86
Luis Trigueiros <luistrigueiros@gmail.com>
2022-08-11 19:09:52 +0100

    Incremental save

--- src/main/java/myproject/Main.java
+++ src/main/java/myproject/Main.java
@@ -1,38 +1,30 @@
 package myproject;
 
-import static myproject.SupportKt.getContainers;
+import static myproject.SupportKt.getTemplate;
 
 import com.pulumi.Pulumi;
 import com.pulumi.core.Output;
 import com.pulumi.kubernetes.apps_v1.Deployment;
 import com.pulumi.kubernetes.apps_v1.DeploymentArgs;
 import com.pulumi.kubernetes.apps_v1.inputs.DeploymentSpecArgs;
-import com.pulumi.kubernetes.core_v1.inputs.PodTemplateSpecArgs;
 import com.pulumi.kubernetes.meta_v1.inputs.LabelSelectorArgs;
-import com.pulumi.kubernetes.meta_v1.inputs.ObjectMetaArgs;
 import java.util.Map;
 
 public class Main {
 
     private static Deployment getDeployment(Map labels) {
-        Deployment deployment = new Deployment("nginx", DeploymentArgs.builder()
+        return new Deployment("nginx", DeploymentArgs.builder()
             .spec(DeploymentSpecArgs.builder()
                 .selector(LabelSelectorArgs.builder()
                     .matchLabels(labels)
                     .build())
                 .replicas(1)
-                .template(PodTemplateSpecArgs.builder()
-                    .metadata(ObjectMetaArgs.builder()
-                        .labels(labels)
-                        .build())
-                    .spec(getContainers()
-                        .build())
-                    .build())
+                .template(getTemplate(labels))
 
                 .build())
             .build());
-        return deployment;
     }
+
 
     public static void main(String[] args) {
         Pulumi.run(ctx -> {
--- src/main/kotlin/myproject/Support.kt
+++ src/main/kotlin/myproject/Support.kt
@@ -3,6 +3,8 @@ package myproject
 import com.pulumi.kubernetes.core_v1.inputs.ContainerArgs
 import com.pulumi.kubernetes.core_v1.inputs.ContainerPortArgs
 import com.pulumi.kubernetes.core_v1.inputs.PodSpecArgs
+import com.pulumi.kubernetes.core_v1.inputs.PodTemplateSpecArgs
+import com.pulumi.kubernetes.meta_v1.inputs.ObjectMetaArgs
 
 fun getContainers(): PodSpecArgs.Builder = PodSpecArgs.builder().containers(
     ContainerArgs.builder()
@@ -15,4 +17,18 @@ fun getContainers(): PodSpecArgs.Builder = PodSpecArgs.builder().containers(
         )
         .build()
 )
+
+fun getTemplate(labels: Map<String, String>): PodTemplateSpecArgs =
+    PodTemplateSpecArgs.builder()
+        .metadata(
+            ObjectMetaArgs.builder()
+                .labels(labels)
+                .build()
+        )
+        .spec(
+            getContainers()
+                .build()
+        ).build()
+
+
 
